d5dc5bc0218e8a1a596a47d5b8d23460afd97b23
avani112 <36026630+avani112@users.noreply.github.com>
2019-04-03 15:34:47 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -11,21 +11,21 @@ var fileButton = document.getElementById("fileButton");
                 uploader.value = per;
                 snapshot.ref.getDownloadURL().then(function(url) {
                     console.log('File available at', url);
+                    frm.addEventListener('submit', (e) => {
+                        e.preventDefault();
+                        db.collection('books').doc(frm.name.value).set({
+                            name: frm.name.value,
+                            publisher: frm.publisher.value,
+                            author: frm.author.value,
+                            category: frm.category.value,
+                            filename: file.name,
+                            url: url
+                        });
+                        frm.reset();
+                    }); 
                 });
             }
         );
-        frm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            db.collection('books').add({
-                name: frm.name.value,
-                publisher: frm.publisher.value,
-                author: frm.author.value,
-                category: frm.category.value,
-                filename: file.name
-            });
-            frm.reset();
-            alert("uploading successful")
-        }); 
+       // alert("uploading successful")
+        
      }); 
-     
-     
