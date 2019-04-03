bb58c10457474b3badb2bf5e76177ad625cd210c
avani112 <36026630+avani112@users.noreply.github.com>
2019-04-03 21:20:51 +0530

    Update download.js

--- download.js
+++ download.js
@@ -1,43 +1,28 @@
 const list = document.querySelector('#book-list');
 const form = document.querySelector('#download-form');
+var bookRef = db.collection("books").doc();
+
+
 
 function render(doc){
     let li = document.createElement('li');
     let name= document.createElement('span');
     let category = document.createElement('span');
-    let url = document.createElement('a');
-    var att = document.createAttribute("href");        
-    // url.setAttributeNode(att);
-    // att.value = doc.data().url;   
-    // li.setAttribute('data-id' , doc.id);
-    // name.textContent = doc.data().name;
-    category.textContent = doc.data().category; 
+    let a = document.createElement('a');
+    a.setAttribute('href', doc.data().url);  
+    name.textContent = doc.data().name;
+    category.textContent = doc.data().category;
+    a.textContent = 'download';
+
     li.appendChild(name);
     li.appendChild(category);
-    li.appendChild(url);
-    list.appendChild(li);
-
-   
+    li.appendChild(a);
+    list.appendChild(li); 
 }
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('books').where('category','==','#category').where('name','==','#name').get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            render(doc);
-        }) 
-    });
-    db.collection('books').onSnapshot(snapshot => {
-        let changes = snapshot.docChanges();
-        changes.forEach(change => {
-                render(change.doc);
-        })
+
+db.collection('books').orderBy('name').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        render(doc);
     });
 });
-form.reset();
 
-
-
-var anchor = document.getElementById("myAnchor");  // Get the <a> element with id="myAnchor"
-var att = document.createAttribute("href");        // Create a "href" attribute
-att.value = "https://www.w3schools.com";            // Set the value of the href attribute
-anchor.setAttributeNode(att);  
