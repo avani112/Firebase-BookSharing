bee8a97684d9f6422d6fc498fd3d62dc36a3eacb
avani <avanigokhale112@gmail.com>
2019-04-04 08:19:28 +0530

    change in javascript code

--- download.js
+++ download.js
@@ -1,41 +1,28 @@
-// function download(){
-//     var fileName = window.AppInventor.getWebViewString();
-//     var storage = firebase.storage();
-//     var pathReference = storage.ref(fileName);
-//     pathReference.getDownloadURL().then(function(url) {
-//         window.AppInventor.setWebViewString(url);
-//     });
-// }
-
 const list = document.querySelector('#book-list');
-const frm = document.querySelector('#download-form');
+const form = document.querySelector('#download-form');
+var bookRef = db.collection("books").doc();
+
+
 
 function render(doc){
     let li = document.createElement('li');
     let name= document.createElement('span');
     let category = document.createElement('span');
-    let div = document.createElement('div');
-
-    li.setAttribute('data-id' , doc.id);
+    let a = document.createElement('a');
+    a.setAttribute('href', doc.data().url);  
     name.textContent = doc.data().name;
     category.textContent = doc.data().category;
-    div.textContent = 'download';
+    a.textContent = 'download';
 
     li.appendChild(name);
     li.appendChild(category);
-    li.appendChild(div);
-    list.appendChild(li);
-
-    frm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        db.collection('books').where('category','==','#category').where('name','==','#name').get().then((snapshot) => {
-            snapshot.docs.forEach(doc => {
-                render(doc);
-            }) 
-        });
-    })
-   
+    li.appendChild(a);
+    list.appendChild(li); 
 }
 
+db.collection('books').orderBy('name').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        render(doc);
+    });
+});
 
-//https://firebasestorage.googleapis.com/v0/b/booksharing-93751.appspot.com/o/best.jpeg?alt=media&token=04fc7708-64c3-44fd-9401-c5d719367748
--- upload.js
+++ upload.js
@@ -1,16 +1,7 @@
 const frm = document.querySelector('#upload-form');
-
-frm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('books').add({
-        name: frm.name.value,
-        publisher: frm.publisher.value,
-        author: frm.author.value,
-        category: frm.category.value
-    });
-});
-    var fileButton = document.getElementById("fileButton");
+var fileButton = document.getElementById("fileButton");
     fileButton.addEventListener('change', function(e){
+        e.preventDefault();
         var file = e.target.files[0];
         var storageRef = firebase.storage().ref('books/' + file.name);
         var task = storageRef.put(file);
@@ -18,11 +9,25 @@ frm.addEventListener('submit', (e) => {
             function progress(snapshot){
                 var per = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                 uploader.value = per;
-            },
-            function complete(){
-                alert("your book is uploaded successfully");
+                snapshot.ref.getDownloadURL().then(function(url) {
+                    console.log('File available at', url);
+                    frm.addEventListener('submit', (e) => {
+                        e.preventDefault();
+                        db.collection('books').doc(frm.name.value).set({
+                            name: frm.name.value,
+                            publisher: frm.publisher.value,
+                            author: frm.author.value,
+                            category: frm.category.value,
+                            filename: file.name,
+                            url: url
+                        });
+                        alert("uploading successful");
+                        frm.reset();
+                    }); 
+                });
             }
         );
-
-    });  
-    frm.reset();
+       
+        
+     }); 
+  
